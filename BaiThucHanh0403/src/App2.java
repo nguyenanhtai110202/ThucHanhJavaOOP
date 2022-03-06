bdc5c4479c665949b823648353083be1a0679812
nguyenanhtai <nguyenanhtai20002@gmail.com>
2022-03-06 16:17:29 +0700

    commit BaiThucHanh0403

---
+++ BaiThucHanh0403/src/App2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class App2 {
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap vao so can kiem tra n:");
+        n = scanner.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("Day la so chan!");
+        }
+        else
+        {
+            System.out.println("Day la so le!");
+        }
+    }
+}
